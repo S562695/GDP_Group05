3df65791f9fd716ebe3dd5cdc822a0c8e8c15f24
Naga Lakshmi Konanki <S562695@nwmissouri.edu>
2023-10-20 13:14:14 -0500

    Creating login page

---
+++ Blood Bank System/Blood Bank System/LoginVC.swift
@@ -0,0 +1,29 @@
+//
+//  LoginVC.swift
+//  Blood Bank System
+//
+//  Created by Konanki,Naga Lakshmi on 10/20/23.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
