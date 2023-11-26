1906e78919973131a7cd556feaf79028c5294b65
Naga Lakshmi Konanki <nagalakshmik1403@gmail.com>
2023-11-26 07:28:30 -0600

    providing code for admin page

--- Blood Bank System/Blood Bank System/AdminLoginVC.swift
+++ Blood Bank System/Blood Bank System/AdminLoginVC.swift
@@ -14,9 +14,6 @@ class AdminLoginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-        self.applyAnimatedGradient()
         LogoAnimationView.animation = LottieAnimation.named("adminImg")
                 LogoAnimationView.loopMode = .loop
                 LogoAnimationView.play(){
@@ -25,17 +22,6 @@ class AdminLoginVC: UIViewController {
                 }
         // Do any additional setup after loading the view.
     }
-    
-    private func applyAnimatedGradient(){
-        let animatedGradient = AnimatedGradientView(frame: view.bounds)
-        animatedGradient.direction = .up
-        animatedGradient.animationValues = [(colors: ["#2BCOE4", "#EAECC6"], .up, .axial),
-                                            (colors: ["#833ab4", "#fd1d1d", "fcb045"], .right, .axial),
-                                            (colors: ["#003973", "#E6E68E"], .down, .axial),
-                                            (colors: ["#1E9600", "#FFF200", "FF0000"], .left, .axial)]
-        view.insertSubview(animatedGradient, at: 0)
-    }
-    
     
     @IBOutlet weak var LogoAnimationView: LottieAnimationView!
     
@@ -51,15 +37,47 @@ class AdminLoginVC: UIViewController {
     
     
     @IBAction func checkUsername(_ sender: UITextField) {
+        
     }
     
     @IBAction func checkPassword(_ sender: UITextField) {
+        
     }
     
     @IBAction func loginClicked(_ sender: UIButton) {
+        let enteredUsername = usernameTF.text ?? ""
+                let enteredPassword = passwordTF.text ?? ""
+
+                if enteredUsername == "admin" && enteredPassword == "password" {
+                    // Successful login
+                    print("Admin login successful!")
+                    self.performSegue(withIdentifier: "homeSegue", sender: self)
+                    // Perform the necessary actions after successful login, such as navigating to the admin dashboard.
+                } else {
+                    // Failed login
+                    print("Invalid username or password. Please try again.")
+                    // You might want to show an alert or update the UI to inform the user about the incorrect credentials.
+                }
     }
     
     @IBAction func resetClicked(_ sender: UIButton) {
+        usernameTF.text = ""
+        passwordTF.text = ""
+        loginBTN.isEnabled = false
+        resetBTN.isEnabled = false
+        passwordTF.isEnabled = false
+    }
+    
+ 
+    
+    private func applyAnimatedGradient(){
+        let animatedGradient = AnimatedGradientView(frame: view.bounds)
+        animatedGradient.direction = .up
+        animatedGradient.animationValues = [(colors: ["#2BCOE4", "#EAECC6"], .up, .axial),
+                                            (colors: ["#833ab4", "#fd1d1d", "fcb045"], .right, .axial),
+                                            (colors: ["#003973", "#E6E68E"], .down, .axial),
+                                            (colors: ["#1E9600", "#FFF200", "FF0000"], .left, .axial)]
+        view.insertSubview(animatedGradient, at: 0)
     }
     
     /*
